8abb4406f547a4b350440073c588ae655542cf6e
sabonerune <102559104+sabonerune@users.noreply.github.com>
2026-01-29 15:57:42 +0900

    feat: TalkEditorとSongEditorを必要になってから非同期的に読み込む

---
+++ src/components/LoadingEditor.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <EngineStartupOverlay :isCompletedInitialStartup="false" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import EngineStartupOverlay from "./EngineStartupOverlay.vue";
+</script>
+
+<style lang="scss" scoped>
+@use "@/styles/variables" as vars;
+
+.container {
+  position: relative;
+  min-height: calc(100vh - #{vars.$menubar-height});
+  overflow: hidden;
+}
+</style>
